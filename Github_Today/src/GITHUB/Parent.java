04558b0e75add72a6d0c11abdc77db776931d016
testingqualitysolution19-admin <testingqualitysolution+a100@gmail.com>
2026-07-01 16:47:55 +0530

    New Class Add

---
+++ Github_Today/src/GITHUB/Parent.java
@@ -0,0 +1,19 @@
+package GITHUB;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Parent {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
